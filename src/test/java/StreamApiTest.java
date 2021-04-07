f82019a8c3e6a025ffd9f88349f3284cb486da73
n-machida <n-machida@ceres-inc.jp>
2021-04-07 20:05:13 +0900

    feat: add map

--- src/test/java/StreamApiTest.java
+++ src/test/java/StreamApiTest.java
@@ -1,6 +1,9 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,5 +24,41 @@ public class StreamApiTest {
         List<Integer> newList = list.stream().filter(l -> l % 2 == 0).collect(Collectors.toList());
 
         assertThat(newList).containsExactly(2, 4);
+    }
+
+    @Test
+    void map化() {
+        List<Person> list = Person.getList();
+        Map<String, Person> collect = list.stream().collect(Collectors.toMap(l -> l.getName(), l -> l));
+
+        assertThat(collect).containsEntry("name", new Person("name", 26));
+        assertThat(collect).containsEntry("name2", new Person("name2", 32));
+        assertThat(collect).containsEntry("name3", new Person("name3", 26));
+    }
+
+    @Test
+    void map化重複削除() {
+        List<Person> list = Person.getList();
+        Map<Integer, Person> collect = list.stream().collect(Collectors.toMap(l -> l.getAge(), l -> l, (a, b) -> {
+            return a;
+        }));
+
+        assertThat(collect).containsEntry(26, new Person("name", 26));
+        assertThat(collect).containsEntry(32, new Person("name2", 32));
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Person {
+        private String name;
+        private int age;
+
+        static List<Person> getList() {
+            return List.of(
+                    new Person("name", 26),
+                    new Person("name2", 32),
+                    new Person("name3", 26)
+            );
+        }
     }
 }
